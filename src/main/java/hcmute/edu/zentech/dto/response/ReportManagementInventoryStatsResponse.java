606241de5ed73967a8e6bfc0214efda874089482
Bùi Đức Lộc <bigbossteamute@gmail.com>
2026-06-12 15:44:41 +0700

    Cập nhật lại logic tính toán số liệu thật

--- src/main/java/hcmute/edu/zentech/dto/response/ReportManagementInventoryStatsResponse.java
+++ src/main/java/hcmute/edu/zentech/dto/response/ReportManagementInventoryStatsResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,4 +16,5 @@ public class ReportManagementInventoryStatsResponse {
     private int totalItemsInStock;
     private int lowStockVariations;
     private int deadStockVariations;
+    private List<ReportManagementProductPerformanceResponse> lowStockProducts;
 }
