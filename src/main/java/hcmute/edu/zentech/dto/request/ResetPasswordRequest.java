7d2f42349be0c8951caee67438397656fa9610f4
Lục Thới Trọng <hunterent1304@gmail.com>
2026-06-26 20:03:38 +0700

    complete refactor code register

--- src/main/java/hcmute/edu/zentech/dto/request/ResetPasswordRequest.java
+++ src/main/java/hcmute/edu/zentech/dto/request/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package hcmute.edu.zentech.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,5 +16,9 @@ public class ResetPasswordRequest {
 
     @NotBlank(message = "Mật khẩu mới không được để trống")
     @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{6,}$",
+            message = "Mat khau phai co it nhat 1 chu viet hoa va 1 ky tu dac biet"
+    )
     private String newPassword;
 }
