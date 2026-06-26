ed1c43c3424ae22414a60146b3789449d888b4bc
Lục Thới Trọng <hunterent1304@gmail.com>
2026-06-26 19:48:30 +0700

    complete refactor register

--- src/main/java/hcmute/edu/zentech/dto/request/RegisterRequest.java
+++ src/main/java/hcmute/edu/zentech/dto/request/RegisterRequest.java
@@ -3,6 +3,8 @@ package hcmute.edu.zentech.dto.request;
 import hcmute.edu.zentech.model.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,6 +14,11 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, message = "Mat khau phai co it nhat 6 ky tu")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{6,}$",
+            message = "Mat khau phai co it nhat 1 chu viet hoa va 1 ky tu dac biet"
+    )
     private String password;
 
     private String fullName;
